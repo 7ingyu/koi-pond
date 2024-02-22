c08cd6d2350520ff9c055e00f3600f8d8c6dbd7a
Ting-Yu Chen <7ingyu@gmail.com>
2024-02-22 00:57:16 -0800

    Added spots to koi, fish change colors after leaving canvas

--- src/components/KoiFish.tsx
+++ src/components/KoiFish.tsx
@@ -3,11 +3,18 @@ import MotionPathPlugin from "gsap/MotionPathPlugin"
 import gsap from "gsap"
 import { generatePath, randomBetween } from "@/utils"
 
-type KoiFishProps = {
-  color: 'gold' | 'red' | 'black' | 'white'
-}
+const colors = [
+  "orange",
+  "white",
+  "white-red",
+  "white-orange",
+  "black",
+  "gold",
+  "gold-black",
+  "red",
+]
 
-const KoiFish = ({ color = 'red' } : KoiFishProps) => {
+const KoiFish = () => {
   const t1 = gsap.timeline()
   const t2 = gsap.timeline()
 
@@ -16,9 +23,9 @@ const KoiFish = ({ color = 'red' } : KoiFishProps) => {
 
   const animate = () => {
     // if (fish?.current) {
-    // const path = generatePath()
-    const height: string = `${randomBetween(5, 10)}vw`
     const path = generatePath()
+    const height: number = randomBetween(5, 10)
+    const width: number = height * 3.258;
     const duration = randomBetween(10, 20)
 
     const animVars = {
@@ -32,22 +39,31 @@ const KoiFish = ({ color = 'red' } : KoiFishProps) => {
         // repeatRefresh: true,
         // resolution: 20
       },
-      height,
+      height: `${height}vw`,
+      width: `${width}vw`,
       duration,
       ease: "none",
       // repeat: started ? -1,
+      onStart: () => {
+        const color = colors[Math.floor(randomBetween(0, colors.length))]
+        fish.current?.setAttribute('class', `koi koi-${color}`)
+        fish.current?.setAttribute('aria-label', `${color} koi fish`)
+      },
       onComplete: () => {
-        tl.clear()
+        t1.clear()
+        t2.clear()
         animate()
       },
     }
 
     t1.to(fish.current, {
-      height,
+      height: `${height}vw`,
+      width: `${width}vw`,
       duration: 0
     })
     t2.to(shadow.current, {
-      height,
+      height: `${height}vw`,
+      width: `${width}vw`,
       duration: 0
     })
 
@@ -66,49 +82,15 @@ const KoiFish = ({ color = 'red' } : KoiFishProps) => {
   return (
     <div className="koi-container">
       <div
-        className="koi"
-        ref={fish}
-      >
-        {/* <svg viewBox="0 0 800 400">
-          <path id='path' style={{stroke: 'red', strokeWidth: '2px', fill: 'none'}}/>
-        </svg> */}
-        <svg
-          className="koi-svg"
-          aria-label={`${color} koi fish`}
-          data-name="Koi Fish"
-          viewBox="0 0 621.31 189.89"
-        >
-          <g
-            data-name="Koi SVG Paths"
-          >
-            <g>
-              <path className={`koi-${color}`} d="M0,94.67c0,21.79,72,14.37,166.98,0C72,80.3,0,72.88,0,94.67Z"/>
-              <path className={`koi-${color}`} d="M506.99,51.41c-4.33-8.82-10.67-17.75-18.82-25.9C463.56.9,431.9-7.33,417.45,7.12c-10.68,10.68-8.96,30.75,2.59,50.43-81.78,8.86-175.15,25.33-253.06,37.12,77.73,11.76,170.84,28.17,252.48,37.05-11.87,19.88-13.73,40.26-2.94,51.04,14.45,14.45,46.11,6.22,70.71-18.38,8.33-8.33,14.77-17.46,19.1-26.47,68.05,1.34,114.98-9.17,114.98-43.25s-46.62-44.52-114.32-43.26Z"/>
-            </g>
-          </g>
-          </svg>
-      </div>
-      <div
-        className="koi-shadow"
+        className={`koi-shadow`}
         ref={shadow}
       >
-        {/* <svg viewBox="0 0 800 400">
-          <path id='path' style={{stroke: 'red', strokeWidth: '2px', fill: 'none'}}/>
-        </svg> */}
-        <svg
-          className="koi-svg"
-          data-name="Koi Fish"
-          viewBox="0 0 621.31 189.89"
-        >
-          <g
-            data-name="Koi SVG Paths"
-          >
-            <g>
-              <path className={`koi-${color}`} d="M0,94.67c0,21.79,72,14.37,166.98,0C72,80.3,0,72.88,0,94.67Z"/>
-              <path className={`koi-${color}`} d="M506.99,51.41c-4.33-8.82-10.67-17.75-18.82-25.9C463.56.9,431.9-7.33,417.45,7.12c-10.68,10.68-8.96,30.75,2.59,50.43-81.78,8.86-175.15,25.33-253.06,37.12,77.73,11.76,170.84,28.17,252.48,37.05-11.87,19.88-13.73,40.26-2.94,51.04,14.45,14.45,46.11,6.22,70.71-18.38,8.33-8.33,14.77-17.46,19.1-26.47,68.05,1.34,114.98-9.17,114.98-43.25s-46.62-44.52-114.32-43.26Z"/>
-            </g>
-          </g>
-        </svg>
+      </div>
+      <div
+        // className={`koi koi-${color}`}
+        ref={fish}
+        // aria-label={`${color} koi fish`}
+      >
       </div>
     </div>
   )
--- src/components/Pond.tsx
+++ src/components/Pond.tsx
@@ -4,20 +4,18 @@
 import { KoiFish, Water, Background } from "@/components"
 import { randomBetween } from "@/utils"
 
-const Pond = ({ numFish = 10 }) => {
+const Pond = ({ numFish = 20 }) => {
 
   // useEffect(() => {
   //   gsap.registerPlugin(MotionPathPlugin)
   // }, [])
-  const colors: ('gold' | 'red' | 'black' | 'white')[] = ['gold', 'black', 'red', 'white']
 
   return (
     <div id="koi-pond">
       {/* <Background /> */}
       <div className="fish-container">
         {new Array(numFish).fill(0).map((_, i) => {
-          const color = colors[Math.floor(randomBetween(0, 4))]
-          return <KoiFish key={i} color={color} />
+          return <KoiFish key={i} />
         })}
       </div>
       <Water />
